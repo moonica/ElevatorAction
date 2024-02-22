f54a915c46f014a03cde2512927031c0fa4f320a
jonicabrown <36988370+jonicabrown@users.noreply.github.com>
2024-02-22 10:43:30 +0200

    Move some commonly used test method code to a utils class

--- ElevatorTests/ConsoleInterfaceTests.cs
+++ ElevatorTests/ConsoleInterfaceTests.cs
@@ -8,35 +8,27 @@ namespace ElevatorTests
         private string input = null;
         private ConsoleInterface ui = new ConsoleInterface();
 
-        private Dictionary<string, string> errorMessages = new Dictionary<string, string>
-        {
-            { "shouldBeExit", "The return command for input {0} should be 'Exit'. {1} received instead." }
-        };
-
-        private void assertCommandHelper(string input, CommandType commandTargetType)
-        {
-            var response = ui.getCommand(input);
-
-            Assert.AreEqual(commandTargetType, response, string.Format(errorMessages["shouldBeExit"], "'null'", response.ToString()));
-        }
-
+        #region EXIT COMMAND TESTS
+        
         [TestMethod]
         public void getCommandReturnsExitOn_null()
         {
-            assertCommandHelper(null, CommandType.Exit);
+            TestUtils.assertCommandHelper(null, CommandType.Exit);
         }
 
         [TestMethod]
         public void getCommandReturnsExitOn_exit_quit()
         {
-            assertCommandHelper("exit", CommandType.Exit);
-            assertCommandHelper("quit", CommandType.Exit);
+            TestUtils.assertCommandHelper("exit", CommandType.Exit);
+            TestUtils.assertCommandHelper("quit", CommandType.Exit);
         }
 
         [TestMethod]
         public void getCommandReturnsExitOn_uppercase()
         {
-            assertCommandHelper("EXIT", CommandType.Exit);
+            TestUtils.assertCommandHelper("EXIT", CommandType.Exit);
         }
+
+        #endregion EXIT COMMAND TESTS
     }
 }
---
+++ ElevatorTests/TestUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElevatorAction.UserInterface;
+
+
+namespace ElevatorTests
+{
+    internal static class TestUtils
+    {
+        private static Dictionary<string, string> errorMessages = new Dictionary<string, string>
+        {
+            { "shouldBeExit", "The return command for input {0} should be 'Exit'. {1} received instead." }
+        };
+
+        public static void assertCommandHelper(string input, CommandType commandTargetType)
+        {
+            var response = ui.getCommand(input);
+
+            Assert.AreEqual(commandTargetType, response, string.Format(errorMessages["shouldBeExit"], "'null'", response.ToString()));
+        }
+    }
+}
