63f5724d30457d03c6cfb2e2d15782c652f8de67
jonicabrown <36988370+jonicabrown@users.noreply.github.com>
2024-02-22 10:16:45 +0200

    Add logic for "TryAgain" command, ie invalid inputs

--- ElevatorMaster.cs
+++ ElevatorMaster.cs
@@ -1,11 +1,14 @@
 ﻿using ElevatorAction.UserInterface;
 using ElevatorAction.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace ElevatorAction
 {
     internal class ElevatorMaster
     {
         private IUserInterface _ui;
+        private readonly IConfiguration _config;
+        private readonly int _retryCount = 5; //default
 
         private readonly List<string> affirmativeInputs = new List<string> { "YES", "Y" };
 
@@ -13,12 +16,19 @@ namespace ElevatorAction
         private Dictionary<string, string> PHRASES = new Dictionary<string, string>
         {
             { "AreYouSure", "Are you sure you want to exit?" },
+            { "RetriesExceeded", "You have entered too many invalid commands, the program will be exited." },
+            { "Retry", "That command was not recognized. Please try again." },
+            { "RetryCountdown", "{0} retries remaining..." },
             { "Welcome", "Welcome to Elevator Action. What would you like to do today?" }
         };
 
-        internal ElevatorMaster(IUserInterface ui)
+        internal ElevatorMaster(IUserInterface ui, IConfiguration config)
         {
             _ui = ui;
+            _config = config;
+
+            //override the retry count with the config value, if present, else leave at the default
+            int.TryParse(Utils.GetConfigSetting(_config, "retryCount"), out _retryCount);
         }
 
         internal async Task Execute()
@@ -38,17 +48,18 @@ namespace ElevatorAction
             }
 
             //once no more commands are received, exit
-            ShutdownWithConfirmation();
+            PerformShutdownWithConfirmation();
         }
 
         private async void PerformCommand(CommandType command)
         {
             switch (command)
             {
-                //case ConsoleCommand.TryAgain:
-                //    break;
+                case CommandType.TryAgain:
+                    await PerformRetry();
+                    break;
                 case CommandType.Exit:
-                    await ShutdownWithConfirmation();
+                    await PerformShutdownWithConfirmation();
                     break;
                 default:
                     _ui.ShutDown();
@@ -56,7 +67,41 @@ namespace ElevatorAction
             }
         }
 
-        private async Task ShutdownWithConfirmation()
+        private async Task PerformRetry()
+        {
+            int retryIdx = 0;
+            var cmd = CommandType.TryAgain;
+            bool success = false;
+            var msg = string.Empty;
+
+            //keep going until we get a command that is recognized, or run out of retries
+            while (((_retryCount == -1) || (retryIdx < _retryCount)) && !success)
+            {
+                //if there's a limit on retries, display how many are left
+                msg = (_retryCount == -1) ? PHRASES["Retry"] : $"{PHRASES["Retry"]} {string.Format(PHRASES["RetryCountdown"], retryIdx)}";
+                _ui.Display(msg);
+
+                //get the new command input
+                cmd = await _ui.GetCommandAsync();
+
+                //if the new input is valid, we can exit the while loop
+                success = (cmd != CommandType.TryAgain);
+            }
+
+            if (success)
+                PerformCommand(cmd);
+            else
+            {
+                if (_retryCount > -1)
+                {
+                    _ui.Display(PHRASES["RetriesExceeded"]);
+                    _ui.ShutDown();
+                }
+                // else: we should never reach this piece of code because either there's no retry limit and we're still in the while, or there was a successful command which was executed in the outer if
+            }
+        }
+
+        private async Task PerformShutdownWithConfirmation()
         {
             _ui.Display(PHRASES["AreYouSure"], true);
             var confirmationResponse = await _ui.GetInputAsync();
