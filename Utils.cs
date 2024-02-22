78a2f34c643d8b5cbdf27121673e620ee1956c6b
jonicabrown <36988370+jonicabrown@users.noreply.github.com>
2024-02-22 16:22:53 +0200

    Move phrase dictionary to utils class

--- Utils.cs
+++ Utils.cs
@@ -9,6 +9,17 @@ namespace ElevatorAction
 {
     public static class Utils
     {
+        //All natural language phrases are enumerated here for ease of change; they could also be put in a localization dictionary per language
+        public static Dictionary<string, string> Phrases_en = new Dictionary<string, string>
+        {
+            { "AreYouSure", "Are you sure you want to exit?" },
+            { "Goodbye", "Goodbye" },
+            { "RetriesExceeded", "You have entered too many invalid commands, the program will be exited." },
+            { "Retry", "That command was not recognized. Please try again, or enter 'Help' to see a list of valid commands." },
+            { "RetryCountdown", "{0} retries remaining..." },
+            { "Welcome", "Welcome to Elevator Action. What would you like to do today?" }
+        };
+
         public static string GetConfigSetting(IConfiguration config, string keyName)
         {
             if (!string.IsNullOrEmpty(keyName))
