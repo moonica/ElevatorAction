e4a07871783509b420a440863df399450b1b85f2
jonicabrown <36988370+jonicabrown@users.noreply.github.com>
2024-02-22 12:21:29 +0200

    Create a test interface to inject into the project to simulate inputs and outputs

---
+++ ElevatorTests/TestInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElevatorAction.UserInterface;
+
+namespace ElevatorTests
+{
+    internal class TestInterface : IUserInterface
+    {
+        public CommandType TestCommand = CommandType.Exit;
+        public string TestInput = string.Empty;
+
+        public void Display(string message, bool isConfirmation = false)
+        {
+            Debug.WriteLine(message);
+        }
+
+        public async Task<CommandType> GetCommandAsync()
+        {
+            return TestCommand;
+        }
+
+        public async Task<string> GetInputAsync()
+        {
+            return TestInput;
+        }
+
+        public void ShutDown()
+        {
+            Debug.WriteLine("Application ended");
+        }
+    }
+}
