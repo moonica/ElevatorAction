9b479363deb0a4c2cd834c3173e352331135108c
jonicabrown <36988370+jonicabrown@users.noreply.github.com>
2024-02-24 16:20:36 +0200

    Add ILogger dependency injection

--- ElevatorMaster.cs
+++ ElevatorMaster.cs
@@ -1,36 +1,52 @@
 ﻿using ElevatorAction.UserInterface;
 using ElevatorAction.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace ElevatorAction
 {
     public class ElevatorMaster
     {
+        private ILogger _logger;
         private IUserInterface _ui;
         private readonly IConfiguration _config;
         private readonly int _retryCount = 5; //default
         private ElevatorController controller;
         private Dictionary<string, string> _phrases = new Dictionary<string, string>();
 
-        private readonly List<string> affirmativeInputs = new List<string> { "YES", "Y" };
-
-        public ElevatorMaster(IUserInterface ui, IConfiguration config, Dictionary<string, string> phrases)
+        public ElevatorMaster(IUserInterface ui, IConfiguration config, ILogger logger, Dictionary<string, string> phrases)
         {
-            _ui = ui;
-            _config = config;
+            try
+            {
+                _ui = ui;
+                _config = config;
+                _logger = logger;
 
-            //Get all phrases from the utils for now, this could come from translation files later
-            _phrases = phrases;
+                //Get all phrases from the utils for now, this could come from translation files later
+                _phrases = phrases;
 
-            //override the retry count with the config value, if present, else leave at the default
-            int.TryParse(Utils.GetConfigSetting(_config, "retryCount"), out _retryCount);
+                //override the retry count with the config value, if present, else leave at the default
+                int.TryParse(Utils.GetConfigSetting(_config, "retryCount"), out _retryCount);
 
-            controller = new ElevatorController(_ui, _retryCount);
+                controller = new ElevatorController(_ui, _retryCount);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, null);
+
+                _ui?.Display($"The application encountered the following unexpected error occurred and will be shut down: '{ex.Message}'");
+
+                if (controller is null)
+                    _ui?.ShutDown();
+                else
+                    controller?.PerformShutdownWithFinalMessage(_phrases[_ui?.PressAnyKeyTranslationKey], true);
+            }
         }
 
         internal async Task Execute()
         {
             _ui.Display(_phrases["Welcome"]);
+            _logger.Log(LogLevel.Information, $"Elevator Action program started [{DateTime.Now.ToString("yyyyMMdd HH:mm:ss")}]");
 
             //get first command
             var input = await _ui.GetCommandAsync();
@@ -52,6 +68,10 @@ namespace ElevatorAction
         {
             switch (command)
             {
+                case CommandType.Test:
+                    //Test command used in unit tests to verify correct switching
+                    _ui.Display("Tested");
+                    break; 
                 case CommandType.TryAgain:
                     await PerformCommand(
                         await controller.GetCommandAfterRetry(_phrases["Retry"], _phrases["RetryCountdown"])
--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
 ﻿using ElevatorAction.UserInterface;
 using ElevatorAction.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace ElevatorAction
 {
     internal class Program
     {
-        /*
-          The use of dependency injection for the user interface is a little contrived here, but as much as possible without over-engineering past the point of the requirements given (a console application per se), I have attempted to decouple the entry point from the console specific interface details. It would need some additional work to truly become loosely coupled but I have attempted to minimise that technical debt, and to illustrate how dependency injection can be generally used for seperation of concerns and clean architecture.
-          -Jonica Brown Feb 2024
-        */
-
-        private static readonly IConfiguration Configuration;
+        private static IConfiguration _configuration;
+        private static ILogger _logger;
 
         static void Main(string[] args)
         {
-            var elevatorMaster = new ElevatorMaster(new ConsoleInterface(), Configuration, Utils.Phrases_en);
-            _ = elevatorMaster.Execute(); //console applications can't really be async, so we can't await the async Execute method
+            var elevatorMaster = new ElevatorMaster(new ConsoleInterface(), _configuration, _logger, Utils.Phrases_en);
+
+            //console applications can't really be async, so we can't await the async Execute method
+            _ = elevatorMaster.Execute(); 
         }
     }
 }
